01a3e23ef94f5e2fba84cfe920b1735af1d2d64f
ashshah <ashna.shah.1426@gmail.com>
2020-07-28 12:09:14 -0700

    modified links, changes made for ip-address and deleted zip file

--- example_script.py
+++ example_script.py
@@ -2,7 +2,7 @@ from napalm import get_network_driver
 
 driver = get_network_driver("sros")
 optional_args = {'port': 830}
-device = driver("138.120.181.55", "netconf", "n37c0nf20", 60, optional_args)
+device = driver("127.0.0.1", "vagrant", "vagrant", 60, optional_args)
 device.open()
 print(device.get_facts())
 print(device.get_optics())
